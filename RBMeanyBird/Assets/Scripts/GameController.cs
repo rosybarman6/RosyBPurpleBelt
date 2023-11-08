26008f37d3f4a220bb39715219d1a8e6961a2d20
MasterHacker10 <noahcho78@gmail.com>
2023-11-08 17:48:07 -0500

    started meany bird
    not finished

---
+++ RBMeanyBird/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class GameController : MonoBehaviour
+{
+    [Header("Game Controller Object for controlling the game")]
+    public GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
