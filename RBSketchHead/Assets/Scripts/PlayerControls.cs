5212a14cddb74924a8d3538966ee1182301e3b88
MasterHacker10 <noahcho78@gmail.com>
2023-12-12 17:36:43 -0500

    Started SketchHead
    Doodlehead kind of stops

--- RBSketchHead/Assets/Scripts/PlayerControls.cs
+++ RBSketchHead/Assets/Scripts/PlayerControls.cs
@@ -5,15 +5,43 @@ using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour
 {
+    [Header("Rigidbody")]
+    public Rigidbody2D rb;
+    [Header("Default Movement Speed")]
+    public float downSpeed = 20f;
+    [Header("Default Movement Speed")]
+    public float movementSpeed = 10f;
+    public float movement = 0f;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+        if (movement < 0)
+        {
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
 
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.velocity = new Vector3(rb.velocity.x, downSpeed, 0);
     }
 }
