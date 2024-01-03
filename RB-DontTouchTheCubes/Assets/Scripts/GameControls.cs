a001fbfd85a29280d2e181a74d2c53fb95f49c9f
MasterHacker10 <noahcho78@gmail.com>
2024-01-03 17:37:35 -0500

    DTTC starting
    xdrctfvgybhun

---
+++ RB-DontTouchTheCubes/Assets/Scripts/GameControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControls: MonoBehaviour
+{
+    private Text timerText;
+    private int timerCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
