05f7a86f704220c510f991207587fe236816c5b7
MasterHacker10 <noahcho78@gmail.com>
2024-03-22 16:57:09 -0400

    Continued DroppingBombs3
    wefratbhgvfbzsnhdbgds

---
+++ PY - Trickhead/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject.Find("DoodleHead").SetActive(false);
+        GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
