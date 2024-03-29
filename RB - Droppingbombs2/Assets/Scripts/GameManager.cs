f219456e1511f1fdb30580e28444bd3ceeacf925
cutoopen <161525484+cutoopen@users.noreply.github.com>
2024-03-29 17:06:27 -0400

    Continued Dropping Bombs 3
    vacdsvasfrgsdge

---
+++ RB - Droppingbombs2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Spawner spawner;
+    public GameObject title:
+    void Awake()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner.active = false;
+        title.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKewDown)
+        {
+            spawner.active = true;
+            title.SetActive(false);
+        }
+    }
+}
