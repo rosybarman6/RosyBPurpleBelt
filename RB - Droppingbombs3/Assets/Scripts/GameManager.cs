d2dc8a0f9075e50eb4c025ef26fc0279f1e16a69
MasterHacker10 <rosybarman6@gmail.com>
2024-04-23 17:01:48 -0400

    Continued Dropping Bombs 3
    curious george washington

--- RB - Droppingbombs3/Assets/Scripts/GameManager.cs
+++ RB - Droppingbombs3/Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,51 +10,34 @@ public class GameManager : MonoBehaviour
     public GameObject playerPrefab;
     private GameObject player;
     private bool gameStarted = false;
-<<<<<<< HEAD
     public GameObject splash;
-=======
->>>>>>> 59ae817b774442a38ff2ddfc25c76879eb514a94
     void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-<<<<<<< HEAD
-=======
-        player = playerPrefab;
->>>>>>> 59ae817b774442a38ff2ddfc25c76879eb514a94
     }
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
         title.SetActive(true);
-        splash.SetActive(false);
+        splash.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!gameStarted)
+        if (Input.anyKeyDown)
         {
-            if (Input.anyKeyDown)
-            {
-                ResetGame();
-            }
-        } else
-        {
-            if (!player)
-            {
-                OnPlayerKilled();
-            }
+            spawner.active = true;
+            title.SetActive(false);
+            splash.SetActive(false);
         }
-<<<<<<< HEAD
 
-=======
->>>>>>> 59ae817b774442a38ff2ddfc25c76879eb514a94
         var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
         foreach (GameObject bombObject in nextBomb)
         {
-            if (bombObject.transform.position.y < (-screenBounds.y) - 12 || !gameStarted)
+            if (bombObject.transform.position.y < (-screenBounds.y) - 12)
             {
                 Destroy(bombObject);
             }
@@ -66,24 +48,10 @@ public class GameManager : MonoBehaviour
     {
         spawner.active = true;
         title.SetActive(false);
+        splash.SetActive(false);
         player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
         gameStarted = true;
     }
-     void OnPlayerKilled()
-    {
-        // when player killed
-        spawner.active = false;
-        gameStarted = false;
-
-        splash.SetActive(true);
-    }
 }
 
-void ResetGame()
-{
-    spawner.active = true;
-    title.SetActive(false);
-    splash.SetActive(false);
-    player.Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotate);
-    gameStarted = true;
-}
+
