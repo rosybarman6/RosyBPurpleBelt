80e1928c8a05d1591ef5ae0dd2d7ee0969b73398
MasterHacker10 <141876965+MasterHacker10@users.noreply.github.com>
2024-05-07 16:40:10 -0400

    Dropping Bombs 3
    feqfdwFDWA

--- RB - Droppingbombs3/Assets/Scripts/GameManager.cs
+++ RB - Droppingbombs3/Assets/Scripts/GameManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private Spawner spawner;
@@ -12,11 +12,17 @@ public class GameManager : MonoBehaviour
     private bool gameStarted = false;
     public GameObject splash;
     public GameObject gameOverText;
+    public GameObject scoreSystem;
+    public Text scoreText;
+    public int pointsWorth = 1;
+    private int score;
+
     void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         player = playerPrefab;
+        scoreText.enabled = false;
     }
     // Start is called before the first frame update
     void Start()
@@ -63,6 +69,10 @@ public class GameManager : MonoBehaviour
         gameOverText.SetActive(false);
         player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
         gameStarted = true;
+
+        scoreText.enabled = true;
+        scoreSystem.GetComponent<Score>().score = 0;
+        scoreSystem.GetComponent<Score>().Start();
     }
 
 
