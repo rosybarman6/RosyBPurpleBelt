0df4fa379b562f00c671b6aff589a145f7b473bb
MasterHacker10 <141876965+MasterHacker10@users.noreply.github.com>
2024-06-07 16:51:56 -0400

    Finished DroppingBombs4 and 5
    HEHEHEHEHEHEHEHEHEHEH BROWN BELT

--- RB - Droppingbombs3/Assets/Scripts/GameManager.cs
+++ RB - Droppingbombs3/Assets/Scripts/GameManager.cs
@@ -17,6 +17,9 @@ public class GameManager : MonoBehaviour
     public Text scoreText;
     public int pointsWorth = 1;
     private bool smokeCleared = true;
+    private int bestScore = 0;
+    public Text bestScoreText;
+    private bool beatBestScore;
 
     private int score;
     void Awake()
@@ -25,6 +28,7 @@ public class GameManager : MonoBehaviour
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         player = playerPrefab;
         scoreText.enabled = false;
+        bestScoreText.enabled = false;
     }
     // Start is called before the first frame update
     void Start()
@@ -34,6 +38,8 @@ public class GameManager : MonoBehaviour
         title.SetActive(true);
         splash.SetActive(true);
         gameOverText.SetActive(false);
+        bestScore = PlayerPrefs.GetInt("BestScore");
+        bestScoreText.text = "Best Score: " + bestScore.ToString();
     }
 
     // Update is called once per frame
@@ -69,6 +75,22 @@ public class GameManager : MonoBehaviour
                 Destroy(bombObject);
             }
         } 
+
+        if (!gameStarted)
+        {
+            var textColor = "#323232";
+
+            if (beatBestScore)
+            {
+                textColor = "#F00";
+            }
+
+            bestScoreText.text = "<color=" + textColor + ">Best Score: " + bestScore.ToString() + "</color>";
+        }
+        else
+        {
+            bestScoreText.text = "";
+        }
     }
 
     void ResetGame()
@@ -83,6 +105,8 @@ public class GameManager : MonoBehaviour
         scoreText.enabled = true;
         scoreSystem.GetComponent<Score>().score = 0;
         scoreSystem.GetComponent<Score>().Start();
+        beatBestScore = false;
+        bestScoreText.enabled = true;
     }
 
 
@@ -93,6 +117,16 @@ public class GameManager : MonoBehaviour
         
        
         Invoke("SplashScreen", 2f);
+
+        score = scoreSystem.GetComponent<Score>().score;
+
+        if(score > bestScore)
+        {
+            bestScore = score;
+            PlayerPrefs.SetInt("BestScore", bestScore);
+            beatBestScore = true;
+            bestScoreText.text = "Best Score: " + bestScore.ToString();
+        }
     }
 
     void SplashScreen()
